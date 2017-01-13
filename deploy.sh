0669f5182048f7f37bd15a091ec58cde7eeb9c01
Odin <odinuv@gmail.com>
2017-01-13 10:54:32 +0100

    fix: image name

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
 # needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars 
 eval $(aws ecr get-login --region us-east-1)
-docker tag keboola/docker-rstudio:latest 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-rstudio:$TRAVIS_TAG
-docker tag keboola/docker-rstudio:latest 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-rstudio:latest
-docker push 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-rstudio:$TRAVIS_TAG
-docker push 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-rstudio:latest
+docker tag keboola/docker-jupyter:latest 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-jupyter:$TRAVIS_TAG
+docker tag keboola/docker-jupyter:latest 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-jupyter:latest
+docker push 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-jupyter:$TRAVIS_TAG
+docker push 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-jupyter:latest
