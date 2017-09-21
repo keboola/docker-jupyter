487c0b957e4c712c4b8a53061ba159b7bdb43f09
Ondrej Popelka <odinuv@gmail.com>
2017-09-21 19:22:50 +0200

    refactor: not used actually

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -27,29 +27,6 @@ c.NotebookApp.allow_root = True
 
 print("Initializing Jupyter", file=sys.stderr)
 
-# Generate a self-signed certificate 
-if 'GEN_CERT' in os.environ:
-    dir_name = jupyter_data_dir()
-    pem_file = os.path.join(dir_name, 'notebook.pem')
-    try:
-        os.makedirs(dir_name)
-    except OSError as exc:  # Python >2.5
-        if exc.errno == errno.EEXIST and os.path.isdir(dir_name):
-            pass
-        else:
-            raise
-    # Generate a certificate if one doesn't exist on disk
-    subprocess.check_call(['openssl', 'req', '-new',
-                           '-newkey', 'rsa:2048',
-                           '-days', '365',
-                           '-nodes', '-x509',
-                           '-subj', '/C=XX/ST=XX/L=XX/O=generated/CN=generated',
-                           '-keyout', pem_file,
-                           '-out', pem_file])
-    # Restrict access to the file
-    os.chmod(pem_file, stat.S_IRUSR | stat.S_IWUSR)
-    c.NotebookApp.certfile = pem_file
-
 # Set a password if PASSWORD is set
 if 'PASSWORD' in os.environ:
     from IPython.lib import passwd
