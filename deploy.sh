5dd9b7f203bcb9451c64d2b3e1e7ff8d94193942
Odin <odinuv@gmail.com>
2017-10-06 21:53:53 +0200

    feat: build on quay too - public repository

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+set -e
+
+docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
+docker tag keboola/docker-jupyter quay.io/keboola/docker-jupyter:${TRAVIS_TAG}
+docker tag keboola/docker-jupyter quay.io/keboola/docker-jupyter:latest
+docker images
+docker push quay.io/keboola/docker-jupyter:${TRAVIS_TAG}
+docker push quay.io/keboola/docker-jupyter:latest
 
 # install aws cli w/o sudo
 pip install --user awscli
