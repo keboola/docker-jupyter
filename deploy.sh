1c3da93996e79d37bd9a653adfb1b82e6fc9422f
Odin <odinuv@gmail.com>
2018-01-23 13:33:36 +0100

    fix: deploy script

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ pip install --user awscli
 # put aws in the path
 export PATH=$PATH:$HOME/.local/bin
 # needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars 
-eval $(aws ecr get-login --region us-east-1)
+eval $(aws ecr get-login --region us-east-1 --no-include-email)
 docker tag keboola/docker-jupyter:latest 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-jupyter:$TRAVIS_TAG
 docker tag keboola/docker-jupyter:latest 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-jupyter:latest
 docker push 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-jupyter:$TRAVIS_TAG
